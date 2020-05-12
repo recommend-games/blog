f6e2eaa3672b800e1f9a6d30f253958e012b37c9
Markus Shepherd <markus.schepke@maas.global>
2020-05-12 09:27:16 +0300

    better plots

--- experiments/sdj.py
+++ experiments/sdj.py
@@ -15,6 +15,8 @@
 
 # %%
 import json
+
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
@@ -41,7 +43,12 @@ sdj = sdj.drop(columns="url").join(games, how="left").sort_values("sdj")
 sdj.shape
 
 # %%
-ksdj = ksdj.drop(columns="url").join(games, how="left").sort_values("ksdj")
+ksdj = (
+    ksdj.drop(index=[203416, 203417])  # only keep on Exit game
+    .drop(columns="url")
+    .join(games, how="left")
+    .sort_values("ksdj")
+)
 ksdj.shape
 
 # %%
@@ -69,16 +76,37 @@ sdj[["sdj"] + columns]
 ksdj[["ksdj"] + columns]
 
 # %%
-sns.lineplot(data=sdj, x="sdj", y="bayes_rating")
+# sns.lineplot(data=sdj, x="sdj", y="bayes_rating")
+plt.plot(sdj.sdj, sdj.bayes_rating, "red")
+plt.plot(ksdj.ksdj, ksdj.bayes_rating, "black")
+plt.show()
 
 # %%
-sns.lineplot(data=sdj, x="sdj", y="complexity")
+# sns.lineplot(data=sdj, x="sdj", y="complexity")
+plt.plot(sdj.sdj, sdj.complexity, "red")
+plt.plot(ksdj.ksdj, ksdj.complexity, "black")
+plt.show()
 
 # %%
-sns.lineplot(data=sdj, x="sdj", y="max_time")
+# sns.lineplot(data=sdj, x="sdj", y="max_time")
+# plt.plot(sdj.sdj, sdj.min_time, "red")
+# plt.plot(sdj.sdj, sdj.max_time, "red")
+plt.fill_between(sdj.sdj, sdj.min_time, sdj.max_time, color="red", alpha=0.25)
+plt.plot(sdj.sdj, (sdj.min_time + sdj.max_time) / 2, "r--")
+# plt.plot(ksdj.ksdj, ksdj.min_time, "black")
+# plt.plot(ksdj.ksdj, ksdj.max_time, "black")
+plt.fill_between(ksdj.ksdj, ksdj.min_time, ksdj.max_time, color="black", alpha=0.25)
+plt.plot(
+    ksdj.ksdj, (ksdj.min_time + ksdj.max_time) / 2, color="black", linestyle="dashed",
+)
+plt.show()
 
 # %%
-sns.lineplot(data=sdj, x="sdj", y="max_players")
+# sns.lineplot(data=sdj, x="sdj", y="max_players")
+plt.fill_between(
+    ksdj.ksdj, ksdj.min_players, ksdj.max_players, color="black", alpha=0.5
+)
+plt.fill_between(sdj.sdj, sdj.min_players, sdj.max_players, color="red", alpha=0.5)
 
 # %%
 games[
@@ -86,5 +114,15 @@ games[
     & (games.year <= 2020)
     & (games.max_time <= 60)
     & (games.complexity <= 2)
+    & (games.max_players >= 3)
+][columns].sort_values("bayes_rating", ascending=False).head(50)
+
+# %%
+games[
+    (games.year >= 2019)
+    & (games.year <= 2020)
+    & (games.max_time <= 120)
+    & (games.complexity >= 1.5)
+    & (games.complexity <= 3.5)
     & (games.max_players >= 3)
 ][columns].sort_values("bayes_rating", ascending=False).head(50)
