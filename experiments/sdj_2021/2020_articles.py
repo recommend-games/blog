d1a1c419bdef1e80c5517e378632a7c705e07f20
Markus Schepke <markus.schepke@wolt.com>
2020-10-26 14:34:43 +0200

    added 2020_articles.csv

--- experiments/sdj_2021/2020_articles.py
+++ experiments/sdj_2021/2020_articles.py
@@ -19,18 +19,40 @@ import re
 
 from pathlib import Path
 
+from pytility import arg_to_iter, parse_int
+
 # %load_ext nb_black
 # %load_ext lab_black
 
 # %%
-regex = re.compile(r"\{\{% game (\d+) %\}\}([^{]+)\{\{% /game %\}\}")
+def games_in_articles(paths):
+    seen = set()
+    for path in arg_to_iter(paths):
+        path = Path(path).resolve()
+        with open(path) as file:
+            for line in file:
+                for match in regex.finditer(line):
+                    bgg_id = parse_int(match.group(1))
+                    name = match.group(2)
+                    if bgg_id and bgg_id not in seen:
+                        seen.add(bgg_id)
+                        yield bgg_id, name, path
+
+
+# %%
+regex = re.compile(r"\{\{%\s*game\s*(\d+)\s*%\}\}([^{]+)\{\{%\s*/game\s*%\}\}")
 
 # %%
 directory = Path("../../content/posts/").resolve()
+out_path = Path("2020_articles.csv").resolve()
 
 # %%
-for path in directory.glob("sdj*/*.md"):
-    with path.open() as file:
-        for line in file:
-            for match in regex.finditer(line):
-                print(match.group(1), match.group(2))
+paths = directory.glob("sdj*/*.md")
+
+# %%
+with out_path.open("w", newline="") as out_file:
+    writer = csv.writer(out_file)
+    writer.writerow(("bgg_id", "name", "comment"))
+    for bgg_id, name, path in games_in_articles(paths):
+        comment = f"From article <{path}>"
+        writer.writerow((bgg_id, name, comment))
