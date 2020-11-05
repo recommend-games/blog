8f02d01f9fb0604c76e7893379ca8b3bbeb72f11
Markus Schepke <markus.schepke@wolt.com>
2020-11-05 12:10:38 +0200

    imported more models

--- experiments/sdj_2021/model.py
+++ experiments/sdj_2021/model.py
@@ -18,6 +18,15 @@ import pandas as pd
 
 from pytility import clear_list
 from sklearn.linear_model import LogisticRegressionCV
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
