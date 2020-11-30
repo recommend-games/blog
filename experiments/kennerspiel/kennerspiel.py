f3eed4f61606b47d9425a1ee2e1b46770b8975ca
Markus Schepke <markus.schepke@wolt.com>
2020-11-30 22:12:34 +0200

    better plot

--- experiments/kennerspiel/kennerspiel.py
+++ experiments/kennerspiel/kennerspiel.py
@@ -23,10 +23,14 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+from bokeh.models import Slope
 from bokeh.plotting import figure, output_notebook, show
+from bokeh.transform import jitter
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.ensemble import RandomForestClassifier
+
+SEED = 23
 
 output_notebook()
 
@@ -78,7 +82,7 @@ data.shape
 data["complexity"][data.complexity.isna()] = 2
 
 # %%
-data.sample(5).T
+data.sample(5, random_state=SEED).T
 
 # %%
 features = [
@@ -97,7 +101,10 @@ features = [
 ] + list(mlb.classes_)
 
 # %%
-lr = LogisticRegressionCV(class_weight="balanced", max_iter=10_000)
+data[features + ["ksdj"]].corr()
+
+# %%
+lr = LogisticRegressionCV(class_weight="balanced", max_iter=10_000, random_state=SEED)
 lr.fit(data[features], data.ksdj)
 
 # %%
@@ -105,63 +112,6 @@ dict(zip(features, lr.coef_[0, :]))
 
 # %%
 lr.score(data[features], data.ksdj)
-
-# %%
-data[features + ["ksdj"]].corr()
-
-# %%
-rfc = RandomForestClassifier(class_weight="balanced", n_jobs=-1)
-rfc.fit(data[features], data.ksdj)
-{
-    feature: importance
-    for importance, feature in sorted(
-        zip(rfc.feature_importances_, features), reverse=True
-    )
-}
-
-
-# %%
-def test_feature_pairs(data=data, features=features, target="ksdj"):
-    for pair in combinations(features, 2):
-        pair = list(pair)  # Pandas column selection requires lists
-        model = LogisticRegressionCV(
-            class_weight="balanced", scoring="f1", max_iter=10_000
-        )
-        model.fit(data[pair], data[target])
-        score = model.score(data[pair], data[target])
-        print(f"{score:.5f}: {pair[0]} & {pair[1]}")
-        yield pair, model, score
-
-
-# %%
-pair, model, score = max(
-    test_feature_pairs(features=set(features) - set(mlb.classes_)), key=lambda x: x[-1]
-)
-print(f"Best score: {score:.5f} for features {pair[0]} & {pair[1]}")
-
-# %%
-TOOLS = "hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select,"
-
-plot = figure(
-    tools=TOOLS,
-    tooltips=[
-        ("name", "@name"),
-        ("year", "@year"),
-        ("complexity", "@complexity"),
-        ("time", "@min_time–@max_time"),
-    ],
-)
-
-data["colors"] = ["#193F4A" if kennerspiel else "#E30613" for kennerspiel in data.ksdj]
-plot.scatter(
-    source=data,
-    x=pair[0],
-    y=pair[1],
-    color="colors",
-    # alpha=0.9,
-    size=8,
-)
-show(plot)
 
 # %%
 data[lr.predict(data[features]) != data.ksdj][
@@ -178,3 +128,81 @@ data[lr.predict(data[features]) != data.ksdj][
         "ksdj",
     ]
 ]
+
+# %%
+rfc = RandomForestClassifier(class_weight="balanced", n_jobs=-1, random_state=SEED)
+rfc.fit(data[features], data.ksdj)
+{
+    feature: importance
+    for importance, feature in sorted(
+        zip(rfc.feature_importances_, features), reverse=True
+    )
+}
+
+
+# %%
+def test_feature_pairs(data=data, features=features, target="ksdj"):
+    for pair in combinations(features, 2):
+        pair = sorted(pair)  # Pandas column selection requires lists
+        model = LogisticRegressionCV(
+            class_weight="balanced",
+            scoring="f1",
+            max_iter=10_000,
+            random_state=SEED,
+        )
+        model.fit(data[pair], data[target])
+        score = model.score(data[pair], data[target])
+        print(f"{score:.5f}: {pair[0]} & {pair[1]}")
+        yield pair, model, score
+
+
+# %%
+pair, model, score = max(
+    test_feature_pairs(features=set(features) - set(mlb.classes_)), key=lambda x: x[-1]
+)
+print(f"Best score: {score:.5f} for features {pair[0]} & {pair[1]}")
+
+# %%
+TOOLS = "hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select"
+
+plot = figure(
+    tools=TOOLS,
+    tooltips=[
+        ("name", "@name"),
+        ("year", "@year"),
+        ("complexity", "@complexity"),
+        ("time", "@min_time–@max_time minutes"),
+        ("age", "@min_age+"),
+    ],
+)
+
+data["color"] = ["#193F4A" if kennerspiel else "#E30613" for kennerspiel in data.ksdj]
+data["marker"] = np.where(model.predict(data[pair]) == data.ksdj, "circle", "square")
+
+plot.scatter(
+    source=data,
+    x=pair[0],
+    y=jitter(pair[1], 0.5),
+    color="color",
+    marker="marker",
+    # alpha=0.9,
+    size=8,
+)
+
+# sigmoid(w*x+b) = 0.5
+# w*x+b = 0
+# w1*x1 + w2*x2 = -b
+# x2 = (-w1*x1-b)/w2
+w1 = model.coef_[0, 0]
+w2 = model.coef_[0, 1]
+b = model.intercept_[0]
+slope = Slope(
+    gradient=-w1 / w2,
+    y_intercept=-b / w2,
+    line_color="black",
+    line_dash="dashed",
+    line_width=2,
+)
+plot.add_layout(slope)
+
+show(plot)
