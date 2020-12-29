e909b35b23ce8c2d1fdee2cfa276322b57bbefc0
Markus Schepke <markus.schepke@wolt.com>
2020-12-29 13:20:14 +0200

    more general charts processing

--- experiments/charts/charts.py
+++ experiments/charts/charts.py
@@ -15,10 +15,11 @@
 
 # %%
 import json
-
+from datetime import datetime, timedelta, timezone
 import pandas as pd
-
 from pytility import parse_date
+
+SEED = 23
 
 # %matplotlib inline
 # %load_ext nb_black
@@ -39,20 +40,20 @@ def process_ratings(
 
 
 # %%
-with open(
-    "../../../board-game-scraper/feeds/bgg/RatingItem/2020-12-28T07-00-47-merged.jl"
-) as file:
-    df = pd.DataFrame.from_records(process_ratings(file), index="updated_at")
+# %%time
+try:
+    df = pd.read_feather("charts.feather").set_index('updated_at')
+except Exception:
+    with open(
+        "../../../board-game-scraper/feeds/bgg/RatingItem/2020-12-28T07-00-47-merged.jl"
+    ) as file:
+        df = pd.DataFrame.from_records(process_ratings(file), index="updated_at")
+    df.reset_index().to_feather("charts.feather")
+    df.to_pickle("charts.pickle")
 df.shape
 
 # %%
-df.sample(25)
-
-# %%
-df.reset_index().to_feather("charts.feather")
-
-# %%
-df.to_pickle("charts.pickle")
+df.sample(25, random_state=SEED)
 
 # %%
 weeks = df.resample("W").item_id.count()
@@ -64,49 +65,58 @@ weeks.plot()
 # %%
 df.bgg_user_rating.hist(bins=10)
 
-# %%
-(df.bgg_user_rating >= 8).mean()
 
 # %%
-cutoff = parse_date("2020-12-01T00:00Z")
-recent = df[df.index >= cutoff]
-recent.bgg_user_rating.describe()
+def calculate_charts(
+    ratings, end_date=None, window=timedelta(days=30), percentiles=(0.25, 0.75)
+):
+    end_date = end_date or datetime.utcnow().replace(tzinfo=timezone.utc)
+    start_date = end_date - window
+
+    previous_ratings = ratings[ratings.index <= end_date]
+    recent_ratings = previous_ratings[previous_ratings.index >= start_date]
+
+    pct_lower, pct_upper = percentiles
+
+    tmp = pd.DataFrame()
+    tmp["positive"] = (
+        recent_ratings[
+            recent_ratings["bgg_user_rating"]
+            >= recent_ratings["bgg_user_rating"].quantile(pct_upper)
+        ]
+        .groupby("bgg_id")["item_id"]
+        .count()
+    )
+    tmp["negative"] = (
+        recent_ratings[
+            recent_ratings["bgg_user_rating"]
+            <= recent_ratings["bgg_user_rating"].quantile(pct_lower)
+        ]
+        .groupby("bgg_id")["item_id"]
+        .count()
+    )
+    tmp.fillna(0, inplace=True)
+    tmp["diff"] = tmp["positive"] - tmp["negative"]
+
+    games = previous_ratings.groupby("bgg_id")["bgg_user_rating"].count()
+    scores = tmp["diff"] * games.rank(pct=True, ascending=False)
+    scores.dropna(inplace=True)
+
+    ranking = pd.DataFrame(
+        data={
+            "rank": scores.rank(ascending=False, method="min").astype(int),
+            "score": scores,
+        },
+        index=scores.index,
+    )
+    return ranking.sort_values("rank")
+
 
 # %%
-positive = (
-    recent[recent.bgg_user_rating >= recent.bgg_user_rating.quantile(0.75)]
-    .groupby("bgg_id")
-    .item_id.count()
-)
-positive.sort_values(ascending=False)[:50]
+# %%time
+charts = calculate_charts(df)
+print(charts.shape)
+charts[:50]
 
 # %%
-negative = (
-    recent[recent.bgg_user_rating <= recent.bgg_user_rating.quantile(0.25)]
-    .groupby("bgg_id")
-    .item_id.count()
-)
-negative.sort_values(ascending=False)[:50]
-
-# %%
-diff = (positive - negative).sort_values(ascending=False)
-diff[:50]
-
-# %% [markdown]
-# **Idea**: Discount that `positive - negative` score by rank according to number of ratings:
-#
-# ```python
-# score = (positive - negative) * games.num_ratings.rank(pct=True)
-# ```
-#
-# That way, games with a lot of ratings will have their value heavily discounted (almost to zero), while games with very few ratings will get the full score.
-
-# %%
-games = pd.read_csv(
-    "../../../board-game-data/scraped/bgg_GameItem.csv", index_col="bgg_id"
-)
-games.shape
-
-# %%
-games["score"] = diff * games.num_votes.rank(pct=True, ascending=False)
-games.sort_values("score", ascending=False)[["name", "year", "score"]][:50]
+charts[-50:]
