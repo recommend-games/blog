85a8500281d16ba2c701596a8c18886f23331ab6
Markus Schepke <markus.schepke@wolt.com>
2021-02-09 08:05:37 +0200

    added playable columns

--- experiments/games.py
+++ experiments/games.py
@@ -38,18 +38,49 @@ def _combine_lists(dataframe):
     return dataframe.apply(_concat_lists, axis=1)
 
 
-def make_transformer(columns, min_df=0.01):
+def _playable_with(dataframe, counts, prefix="", more_column=False):
+    min_col, max_col = dataframe.columns
+    result = pd.DataFrame(index=dataframe.index)
+
+    count = 0  # just in case counts is empty
+    for count in counts:
+        playable = (dataframe[min_col] <= count) & (dataframe[max_col] >= count)
+        column = f"{prefix}{count:02d}"
+        result[column] = playable
+
+    if more_column:
+        playable = dataframe[max_col] > count
+        column = f"{prefix}{count+1:02d}+"
+        result[column] = playable
+
+    return result
+
+
+def make_transformer(
+    list_columns,
+    player_count_columns=("min_players", "max_players"),
+    min_df=0.01,
+):
     """Game transformer."""
 
-    pipeline = make_pipeline(
+    list_pipeline = make_pipeline(
         FunctionTransformer(_list_dataframe),
         FunctionTransformer(_combine_lists),
         CountVectorizer(analyzer=set, min_df=min_df, binary=True, dtype=np.uint8),
         FunctionTransformer(csr_matrix.todense),
     )
+    playable_transformer = FunctionTransformer(
+        _playable_with,
+        kw_args={
+            "counts": range(1, 11),
+            "prefix": "playable_with_",
+            "more_column": True,
+        },
+    )
 
     transformer = make_column_transformer(
-        (pipeline, clear_list(arg_to_iter(columns))),
+        (list_pipeline, clear_list(arg_to_iter(list_columns))),
+        (playable_transformer, clear_list(arg_to_iter(player_count_columns))),
         remainder="passthrough",
     )
 
@@ -59,22 +90,46 @@ def make_transformer(columns, min_df=0.01):
 def matrix_to_dataframe(matrix, transformer, original_columns, index=None):
     """Take the transformed matrix and turn it back into a dataframe."""
 
-    _, pipeline, old_columns = transformer.transformers_[0]
-    vectorizer = pipeline.named_steps.countvectorizer
-    _, transformed_columns = zip(
+    _, list_pipeline, old_list_columns = transformer.transformers_[0]
+    _, playable_transformer, old_playable_columns = transformer.transformers_[1]
+    old_columns = frozenset(old_list_columns + old_playable_columns)
+
+    vectorizer = list_pipeline.named_steps.countvectorizer
+    _, transformed_list_columns = zip(
         *sorted((v, k) for k, v in vectorizer.vocabulary_.items())
     )
-    columns = transformed_columns + tuple(
-        column for column in original_columns if column not in old_columns
+
+    prefix = playable_transformer.kw_args["prefix"]
+    counts = playable_transformer.kw_args["counts"]
+    more_column = playable_transformer.kw_args.get("more_column")
+    transformed_playable_columns = [f"{prefix}{count:02d}" for count in counts]
+    if more_column:
+        count = max(counts)
+        transformed_playable_columns.append(f"{prefix}{count+1:02d}+")
+
+    columns = (
+        list(transformed_list_columns)
+        + transformed_playable_columns
+        + [column for column in original_columns if column not in old_columns]
     )
+
     dataframe = pd.DataFrame(data=matrix, columns=columns, index=index)
     return dataframe.infer_objects()
 
 
-def transform(data, columns, min_df=0.01):
+def transform(
+    data,
+    list_columns,
+    player_count_columns=("min_players", "max_players"),
+    min_df=0.01,
+):
     """Transform the data."""
 
-    transformer = make_transformer(columns=columns, min_df=min_df)
+    transformer = make_transformer(
+        list_columns=list_columns,
+        player_count_columns=player_count_columns,
+        min_df=min_df,
+    )
     matrix = transformer.fit_transform(data)
     return matrix_to_dataframe(
         matrix=matrix,
