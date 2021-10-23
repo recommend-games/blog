eafc96403726bbece2456fb7d0d15ae6c96db780
Markus Schepke <markus.schepke@wolt.com>
2021-10-23 07:25:33 +0300

    load one hotness per day

--- experiments/aggregated_hotness/hotness.py
+++ experiments/aggregated_hotness/hotness.py
@@ -14,8 +14,12 @@
 # ---
 
 # %%
+from datetime import timezone
+from itertools import groupby
 from pathlib import Path
 import pandas as pd
+import numpy as np
+from pytility import parse_date
 
 # %load_ext nb_black
 # %load_ext lab_black
@@ -24,8 +28,28 @@ import pandas as pd
 hot_dir = Path("../../../board-game-data/rankings/bgg/hotness/").resolve()
 hot_dir
 
+
 # %%
-pd.read_csv(
-    max(hot_dir.glob("*.csv")),
-    index_col="bgg_id",
-)["rank"]
+def load_hotness(path, start, end):
+    for d, group in groupby(
+        sorted(
+            (d, p)
+            for p in path.glob("*.csv")
+            if start <= (d := parse_date(p.stem, tzinfo=timezone.utc)) < end
+        ),
+        key=lambda x: x[0].date(),
+    ):
+        _, p = max(group)
+        s = pd.read_csv(
+            p,
+            index_col="bgg_id",
+        )["rank"]
+        yield d, s
+
+
+# %%
+rankings = load_hotness(
+    path=hot_dir,
+    start=parse_date("2021-08-01T00:00Z"),
+    end=parse_date("2021-10-01T00:00Z"),
+)
