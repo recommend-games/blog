32ac1714cf16c1140de89f8e0df4d16c99708a97
Markus Schepke <markus.schepke@wolt.com>
2023-01-13 14:41:35 +0200

    adding headings and overpriced cards

--- experiments/living_forest/living_forest.py
+++ experiments/living_forest/living_forest.py
@@ -13,6 +13,9 @@
 #     name: python3
 # ---
 
+# %% [markdown]
+# # Living Forest card costs
+
 # %%
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -23,6 +26,9 @@ pd.options.display.max_rows = 150
 # %load_ext nb_black
 # %load_ext lab_black
 
+# %% [markdown]
+# ## Data
+
 # %%
 data = pd.read_csv("cards.csv", index_col="name")
 data["flames"] = data["stage"] + 1
@@ -30,6 +36,9 @@ data.shape
 
 # %%
 data.sample(10)
+
+# %% [markdown]
+# ## Model training
 
 # %%
 elements = ["white", "sun", "water", "plant", "spiral", "lotus"]
@@ -47,3 +56,13 @@ model.intercept_
 # %%
 for element, price in zip(elements, model.coef_):
     print(f"{element}:\t{price:6.3f}")
+
+# %% [markdown]
+# ## Over and underpriced cards
+
+# %%
+data["over_price"] = data["price"] - model.predict(data[elements])
+data.sort_values("over_price")
+
+# %%
+# TODO what would be the costs of starting cards?
