c10f17796cb3506d9bda71ce60b908a227ef73b2
Markus Schepke <markus.schepke@wolt.com>
2023-09-25 21:54:38 +0300

    introduced synthetic_control/data.py

--- experiments/synthetic_control/model.py
+++ experiments/synthetic_control/model.py
@@ -16,9 +16,11 @@
 # %% [markdown]
 # # Synthetic Control
 
+# %% [markdown]
+# ## Init and Config
+
 # %%
 import warnings
-from dataclasses import dataclass
 from datetime import date, datetime, timedelta
 from pathlib import Path
 
@@ -30,110 +32,15 @@ from matplotlib import pyplot as plt
 from scipy.optimize import fmin_slsqp
 from sklearn.linear_model import LinearRegression, Ridge
 
+from synthetic_control.data import REVIEWS
+
 jupyter_black.load()
 np.set_printoptions(suppress=True)
 sns.set_style("dark")
 warnings.filterwarnings("ignore")
 
-
-# %% [markdown]
-# ## The Games
-
 # %%
-@dataclass(frozen=True, kw_only=True)
-class GameData:
-    bgg_id: int
-    name: str
-    date_review: date | datetime
-    days_before: int = 60
-    days_after: int = 30
-    max_control_games: int = 300
-
-
-# %%
-games = [
-    GameData(
-        bgg_id=354568,
-        name="Amun-Re",
-        date_review=date(2023, 9, 22),
-    ),
-    GameData(
-        bgg_id=11,
-        name="Bohnanza",
-        date_review=date(2023, 9, 14),
-    ),
-    GameData(
-        bgg_id=311031,
-        name="Five Three Five",
-        date_review=date(2023, 8, 23),
-    ),
-    GameData(
-        bgg_id=298383,
-        name="Golem",
-        date_review=date(2023, 8, 17),
-    ),
-    GameData(
-        bgg_id=386937,
-        name="Lacuna",
-        date_review=date(2023, 8, 3),
-    ),
-    GameData(
-        bgg_id=331571,
-        name="My Gold Mine",
-        date_review=date(2023, 7, 19),
-    ),
-    GameData(
-        bgg_id=367771,
-        name="Stomp the Plank",
-        date_review=date(2023, 7, 6),
-    ),
-    GameData(
-        bgg_id=177478,
-        name="IKI",
-        date_review=date(2023, 6, 29),
-    ),
-    GameData(
-        bgg_id=362944,
-        name="War of the Ring: The Card Game",
-        date_review=date(2023, 6, 15),
-    ),
-    GameData(
-        bgg_id=281549,
-        name="Beast",
-        date_review=date(2023, 6, 9),
-    ),
-    GameData(
-        bgg_id=276086,
-        name="Hamlet",
-        date_review=date(2023, 5, 25),
-    ),
-    GameData(
-        bgg_id=267609,
-        name="Guards of Atlantis II",
-        date_review=date(2023, 5, 18),
-    ),
-    GameData(
-        bgg_id=295770,
-        name="Frosthaven",
-        date_review=date(2023, 5, 11),
-    ),
-    GameData(
-        bgg_id=350205,
-        name="Horseless Carriage",
-        date_review=date(2023, 2, 9),
-    ),
-    GameData(
-        bgg_id=811,
-        name="Rummikub",
-        date_review=date(2023, 1, 26),
-    ),
-    GameData(
-        bgg_id=366013,
-        name="Heat: Pedal to the Metal",
-        date_review=date(2022, 12, 22),
-    ),
-]
-game = games[2]
+game = REVIEWS[2]
 game
 
 # %%
