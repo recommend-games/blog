1a8d40dd187d90f3b228109e37b31471f081e000
Markus Schepke <markus.schepke@wolt.com>
2023-10-02 22:12:08 +0300

    added synthetic_control.models.train_test_split

--- experiments/synthetic_control/synthetic_control/models.py
+++ experiments/synthetic_control/synthetic_control/models.py
@@ -1,16 +1,42 @@
+from typing import Optional
 import numpy as np
+import polars as pl
 from scipy.optimize import fmin_slsqp
+from sklearn.base import BaseEstimator
 from sklearn.linear_model import LinearRegression, Ridge
 
+from synthetic_control.data import GameData
 
-def train_weights_lr(X_train, y_train, model=None):
+
+def train_test_split(
+    data: pl.DataFrame,
+    game: GameData,
+) -> tuple[pl.DataFrame, pl.DataFrame]:
+    data_train_test = data.with_columns(
+        pl.when(pl.col("timestamp") < game.date_review)
+        .then(pl.lit("train"))
+        .otherwise(pl.lit("test"))
+        .alias("train_test")
+    ).partition_by("train_test", as_dict=True)
+
+    return (
+        data_train_test["train"].sort("timestamp"),
+        data_train_test["test"].sort("timestamp"),
+    )
+
+
+def train_weights_lr(
+    X_train: np.ndarray,
+    y_train: np.ndarray,
+    model: Optional[BaseEstimator] = None,
+) -> np.ndarray:
     if model is None:
         model = LinearRegression(fit_intercept=False)
 
     return model.fit(X_train, y_train).coef_
 
 
-def train_weights_slsqp(X_train, y_train):
+def train_weights_slsqp(X_train: np.ndarray, y_train: np.ndarray) -> np.ndarray:
     def loss(weights):
         return np.sqrt(np.mean((y_train - X_train.dot(weights)) ** 2))
 
@@ -25,7 +51,12 @@ def train_weights_slsqp(X_train, y_train):
     )
 
 
-def weights_and_predictions(model, X_train, y_train, X_test):
+def weights_and_predictions(
+    model: str,
+    X_train: np.ndarray,
+    y_train: np.ndarray,
+    X_test: np.ndarray,
+) -> tuple[np.ndarray, np.ndarray]:
     if model == "linear":
         weights = train_weights_lr(
             X_train,
