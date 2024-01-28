a153dda68b53294cbc3d3614826ae89f59d8ef9f
Markus Schepke <markus.schepke@wolt.com>
2024-01-28 23:30:54 +0200

    first version of High Society.py

---
+++ experiments/high_society/High Society.py
@@ -0,0 +1,55 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import jupyter_black
+import numpy as np
+import seaborn as sns
+from scipy.stats import hypergeom
+
+jupyter_black.load()
+
+# %%
+num_cards = 16
+num_dark = 4
+num_games = 1_000_000
+random_seed = None
+
+# %%
+rng = np.random.default_rng(seed=random_seed)
+
+# %%
+games = np.zeros((num_games, num_cards), dtype=bool)
+games[:, :num_dark] = True
+games = rng.permuted(games, axis=1, out=games)
+
+# %%
+lengths = np.sum(np.cumsum(games, axis=1) < num_dark, axis=1)
+print(lengths.mean(), lengths.std())
+game_length, game_length_count = np.unique(lengths, return_counts=True)
+dict(zip(game_length, game_length_count / num_games))
+
+# %%
+cum_probs = hypergeom.pmf(
+    num_dark,
+    num_cards,
+    num_dark,
+    np.arange(num_dark - 1, num_cards + 1),
+)
+probs = cum_probs[1:] - cum_probs[:-1]
+dict(zip(game_length, probs))
+
+# %%
+sns.barplot(x=np.arange(num_dark - 1, num_cards), y=probs)
