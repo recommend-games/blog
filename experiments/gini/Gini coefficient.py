95eb821bff827c0878195bfc0e39e7cdc7e10332
Markus Schepke <markus.schepke@wolt.com>
2024-03-24 22:47:47 +0200

    Added Gini coefficients for all dates and save to CSV

--- experiments/gini/Gini coefficient.py
+++ experiments/gini/Gini coefficient.py
@@ -14,41 +14,66 @@
 # ---
 
 # %%
-# import warnings
+from datetime import datetime
 from pathlib import Path
 import polars as pl
 import jupyter_black
 
-# import seaborn as sns
-# from matplotlib import pyplot as plt
-
 jupyter_black.load()
-# warnings.filterwarnings("ignore")
 
 # %%
-rankings_path = Path(
-    "../../../bgg-ranking-historicals/2024-03-24T00-43-41.csv"
-).resolve()
-rankings_path
+rankings_dir = Path("../../../bgg-ranking-historicals").resolve()
+rankings_dir
+
+
+# %%
+def scan_rankings(path, date_fmt="%Y-%m-%dT%H-%M-%S"):
+    pub_date = datetime.strptime(path.stem, date_fmt)
+    return (
+        pl.scan_csv(path)
+        .select(
+            pl.col("ID").alias("bgg_id"),
+            pl.col("Users rated").alias("num_ratings"),
+        )
+        .with_columns(pl.lit(pub_date).alias("date"))
+    )
+
+
+# %%
+paths = sorted(rankings_dir.glob("*.csv"))
+dfs = map(scan_rankings, paths)
+all_rankings = pl.concat(dfs, how="vertical", parallel=True)
 
 # %%
 gini = (
-    pl.scan_csv(rankings_path)
-    .select(pl.col("Users rated").alias("num_ratings"))
-    .sort("num_ratings")
+    all_rankings.group_by_dynamic("date", every="1d", by="bgg_id")
+    .agg(pl.col("num_ratings").max())
+    .select(pl.col("num_ratings"), pl.col("date").dt.date())
+    .select(pl.all().sort_by("num_ratings").over("date"))
     .with_columns(
-        pl.col("num_ratings").rank(method="ordinal").alias("rank"),
-        pl.col("num_ratings").cum_sum().alias("cum_num_ratings"),
-        pl.col("num_ratings").sum().alias("total_ratings"),
-        pl.len().alias("num_games"),
+        pl.col("num_ratings").rank(method="ordinal").over("date").alias("rank"),
+        pl.col("num_ratings").cum_sum().over("date").alias("cum_num_ratings"),
+        pl.col("num_ratings").sum().over("date").alias("total_ratings"),
+        pl.len().over("date").alias("num_games"),
     )
     .with_columns(
         (pl.col("cum_num_ratings") / pl.col("total_ratings")).alias("share_ratings"),
         (pl.col("rank") / pl.col("num_games")).alias("equal_share"),
     )
     .with_columns((pl.col("equal_share") - pl.col("share_ratings")).alias("diff"))
-    .select(2 * pl.col("diff").mean())
+    .group_by(pl.col("date"))
+    .agg((2 * pl.col("diff").mean()).alias("gini"))
+    .sort(pl.col("date"))
     .collect()
-    .item()
 )
-gini
+
+gini.shape
+
+# %%
+gini.head()
+
+# %%
+gini.tail()
+
+# %%
+gini.write_csv("gini.csv")
