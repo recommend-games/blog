f16e2513ece49214ad327d6c8695754c8da804c0
Markus Schepke <markus.schepke@wolt.com>
2024-04-09 14:09:44 +0300

    Minor updates

--- experiments/board_game_map/games_by_mechanic.py
+++ experiments/board_game_map/games_by_mechanic.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.15.2
+#       jupytext_version: 1.16.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -30,7 +30,6 @@ import umap.plot
 
 from bokeh.plotting import show, save, output_notebook, output_file
 from bokeh.resources import INLINE
-from pytility import parse_date
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LinearRegression
 
