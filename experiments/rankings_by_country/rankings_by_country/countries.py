633bde5fb3827f5908e36bf92a8fc645727cffa0
Markus Schepke <markus.schepke@wolt.com>
2024-04-21 17:10:53 +0300

    Added get_flag_emoji()

--- experiments/rankings_by_country/rankings_by_country/countries.py
+++ experiments/rankings_by_country/rankings_by_country/countries.py
@@ -2,6 +2,7 @@ from functools import cache
 import json
 import logging
 from pathlib import Path
+import flag
 from country_converter import CountryConverter
 
 LOGGER = logging.getLogger(__name__)
@@ -24,3 +25,14 @@ def get_country_code(country_name: str) -> str | None:
     if country:
         return country
     return CONVERTER.convert(names=country_name, to="ISO2", not_found="") or None
+
+
+@cache
+def get_flag_emoji(country_code: str | None) -> str | None:
+    if not country_code or len(country_code) != 2:
+        return None
+    try:
+        return flag.flag(country_code)
+    except (KeyError, ValueError, AttributeError):
+        pass
+    return None
