7b93459b7e72743f7bbc0416e27bac9b6e87c35a
Markus Schepke <markus.schepke@wolt.com>
2024-07-21 14:32:37 +0300

    Added Data.py

---
+++ experiments/generate_bg_titles/Data.py
@@ -0,0 +1,40 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.3
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import json
+import jupyter_black
+from tqdm import tqdm
+
+jupyter_black.load()
+
+# %%
+in_path = "../../../board-game-data/scraped/bgg_GameItem.jl"
+out_path = "./titles.txt"
+
+
+# %%
+def load_titles(path):
+    with open(path, encoding="utf-8") as file:
+        for line in file:
+            game = json.loads(line)
+            if name := game.get("name"):
+                yield f"{name}\n"
+
+
+# %%
+titles = load_titles(path)
+with open(out_path, mode="w", encoding="utf-8") as file:
+    file.writelines(tqdm(titles))
