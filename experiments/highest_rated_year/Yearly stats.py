1226703bce6aaa0831bdc6b6b4bb19b3e70b6778
Markus Schepke <markus.schepke@wolt.com>
2024-08-12 23:14:11 +0300

    Better plotting

--- experiments/highest_rated_year/Yearly stats.py
+++ experiments/highest_rated_year/Yearly stats.py
@@ -15,6 +15,7 @@
 
 # %%
 from datetime import date
+from matplotlib import pyplot as plt
 from pathlib import Path
 import jupyter_black
 import polars as pl
@@ -22,18 +23,21 @@ import seaborn as sns
 import statsmodels.api as sm
 
 jupyter_black.load()
-
 pl.Config.set_tbl_rows(100)
+sns.set_style("dark")
 
 # %%
 this_year = date.today().year
+seed = this_year
 
 # %%
 base_dir = Path(".").resolve()
+plot_dir = base_dir / "plots"
+plot_dir.mkdir(parents=True, exist_ok=True)
 project_dir = base_dir.parent.parent
 rankings_dir = project_dir.parent / "bgg-ranking-historicals"
 data_dir = project_dir.parent / "board-game-data"
-base_dir, project_dir, rankings_dir, data_dir
+base_dir, plot_dir, project_dir, rankings_dir, data_dir
 
 # %% [markdown]
 # # Rankings
@@ -79,7 +83,22 @@ results_from_rankings = model_from_rankings.fit()
 results_from_rankings.summary().tables[1]
 
 # %%
-sns.regplot(data=data_from_rankings, x="year", y="avg_rating", ci=95)
+_, ax = plt.subplots(figsize=(6, 4))
+# TODO: circle size by number of games
+sns.regplot(
+    data=data_from_rankings,
+    x="year",
+    y="avg_rating",
+    ci=95,
+    color="purple",
+    seed=seed,
+    ax=ax,
+)
+ax.set_title("Yearly average ratings from ranked games")
+plt.tight_layout()
+plt.savefig(plot_dir / "avg_ratings_from_rankings.png")
+plt.savefig(plot_dir / "avg_ratings_from_rankings.svg")
+plt.show()
 
 # %% [markdown]
 # # Ratings
@@ -125,4 +144,19 @@ results_from_ratings = model_from_ratings.fit()
 results_from_ratings.summary().tables[1]
 
 # %%
-sns.regplot(data=data_from_ratings, x="year", y="avg_rating", ci=95)
+_, ax = plt.subplots(figsize=(6, 4))
+# TODO: circle size by number of ratings
+sns.regplot(
+    data=data_from_ratings,
+    x="year",
+    y="avg_rating",
+    ci=95,
+    color="purple",
+    seed=seed,
+    ax=ax,
+)
+ax.set_title("Yearly average ratings")
+plt.tight_layout()
+plt.savefig(plot_dir / "avg_ratings_from_ratings.png")
+plt.savefig(plot_dir / "avg_ratings_from_ratings.svg")
+plt.show()
