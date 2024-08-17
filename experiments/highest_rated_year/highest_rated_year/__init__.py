e51a210b53345a1873d81d6e86c1db5b74dfed8a
Markus Schepke <markus.schepke@wolt.com>
2024-08-17 23:53:25 +0300

    Moved load_years_from_rankings() and load_years_from_ratings() to highest_rated_year.data

---
+++ experiments/highest_rated_year/highest_rated_year/__init__.py
@@ -0,0 +1,9 @@
+from highest_rated_year.data import load_years_from_rankings, load_years_from_ratings
+from highest_rated_year.plots import plot_average, save_plots
+
+__all__ = [
+    "load_years_from_rankings",
+    "load_years_from_ratings",
+    "plot_average",
+    "save_plots",
+]
