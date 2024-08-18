571a596bafe9998fe36095a32b0f8290e57952bd
Markus Schepke <markus.schepke@wolt.com>
2024-08-18 23:06:27 +0300

    Use t_test() in Stats.py

--- experiments/highest_rated_year/Stats.py
+++ experiments/highest_rated_year/Stats.py
@@ -18,8 +18,10 @@ from pathlib import Path
 
 import jupyter_black
 import polars as pl
-import statsmodels.api as sm
-from scipy.stats import t
+# import statsmodels.api as sm
+# from scipy.stats import t
+
+from highest_rated_year import t_test
 
 jupyter_black.load()
 pl.Config.set_tbl_rows(100)
@@ -38,41 +40,18 @@ data_from_rankings = years_from_rankings.filter(pl.col("year") >= 1970)
 years_from_rankings.shape, data_from_rankings.shape
 
 # %%
-years = sm.add_constant(data_from_rankings["year"].to_numpy())
-model_from_avg_rankings = sm.OLS(
-    endog=data_from_rankings["avg_rating"].to_numpy(),
-    exog=years,
-).fit()
-model_from_avg_rankings.summary().tables[1]
-
-# %%
-alpha = 0.05  # / len(data_from_rankings)
-avg_rating_pred = model_from_avg_rankings.predict(years)
-influence = model_from_avg_rankings.get_influence()
-data_from_rankings = data_from_rankings.with_columns(
-    avg_rating_pred=avg_rating_pred
-).with_columns(
-    avg_rating_error=pl.col("avg_rating") - pl.col("avg_rating_pred"),
-    avg_rating_error_studentized=influence.resid_studentized_internal,
-)
-data_from_rankings = data_from_rankings.with_columns(
-    avg_rating_p_value=t.sf(
-        data_from_rankings["avg_rating_error_studentized"].abs(),
-        df=model_from_avg_rankings.df_resid,
-    )
-).with_columns(avg_rating_sig=pl.col("avg_rating_p_value") < alpha)
+data_from_rankings = t_test(data_from_rankings, y_column="avg_rating")
 data_from_rankings.shape
 
 # %%
-data_from_rankings.sort(pl.col("avg_rating_p_value").abs()).head(10)
+data_from_rankings.sort(pl.col("avg_rating_p_value")).head(10)
 
 # %%
-model_from_bayes_rankings = sm.OLS(
-    data_from_rankings["bayes_rating"].to_numpy(),
-    sm.add_constant(data_from_rankings["year"].to_numpy()),
-)
-results_from_bayes_rankings = model_from_bayes_rankings.fit()
-results_from_bayes_rankings.summary().tables[1]
+data_from_rankings = t_test(data_from_rankings, y_column="bayes_rating")
+data_from_rankings.shape
+
+# %%
+data_from_rankings.sort(pl.col("bayes_rating_p_value")).head(10)
 
 # %% [markdown]
 # # Ratings
@@ -83,9 +62,8 @@ data_from_ratings = years_from_ratings.filter(pl.col("year") >= 1970)
 years_from_ratings.shape, data_from_ratings.shape
 
 # %%
-model_from_ratings = sm.OLS(
-    data_from_ratings["avg_rating"].to_numpy(),
-    sm.add_constant(data_from_ratings["year"].to_numpy()),
-)
-results_from_ratings = model_from_ratings.fit()
-results_from_ratings.summary().tables[1]
+data_from_ratings = t_test(data_from_ratings, y_column="avg_rating")
+data_from_ratings.shape
+
+# %%
+data_from_ratings.sort(pl.col("avg_rating_p_value")).head(10)
