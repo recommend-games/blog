2ee68122961e5bc56bf8a9210cd15c3410355a1e
Markus Schepke <markus.schepke@wolt.com>
2024-08-18 22:35:01 +0300

    Added some statistical testing to data_from_rankings

--- experiments/highest_rated_year/Stats.py
+++ experiments/highest_rated_year/Stats.py
@@ -19,6 +19,7 @@ from pathlib import Path
 import jupyter_black
 import polars as pl
 import statsmodels.api as sm
+from scipy.stats import t
 
 jupyter_black.load()
 pl.Config.set_tbl_rows(100)
@@ -37,12 +38,33 @@ data_from_rankings = years_from_rankings.filter(pl.col("year") >= 1970)
 years_from_rankings.shape, data_from_rankings.shape
 
 # %%
+years = sm.add_constant(data_from_rankings["year"].to_numpy())
 model_from_avg_rankings = sm.OLS(
-    data_from_rankings["avg_rating"].to_numpy(),
-    sm.add_constant(data_from_rankings["year"].to_numpy()),
+    endog=data_from_rankings["avg_rating"].to_numpy(),
+    exog=years,
+).fit()
+model_from_avg_rankings.summary().tables[1]
+
+# %%
+alpha = 0.05  # / len(data_from_rankings)
+avg_rating_pred = model_from_avg_rankings.predict(years)
+influence = model_from_avg_rankings.get_influence()
+data_from_rankings = data_from_rankings.with_columns(
+    avg_rating_pred=avg_rating_pred
+).with_columns(
+    avg_rating_error=pl.col("avg_rating") - pl.col("avg_rating_pred"),
+    avg_rating_error_studentized=influence.resid_studentized_internal,
 )
-results_from_avg_rankings = model_from_avg_rankings.fit()
-results_from_avg_rankings.summary().tables[1]
+data_from_rankings = data_from_rankings.with_columns(
+    avg_rating_p_value=t.sf(
+        data_from_rankings["avg_rating_error_studentized"].abs(),
+        df=model_from_avg_rankings.df_resid,
+    )
+).with_columns(avg_rating_sig=pl.col("avg_rating_p_value") < alpha)
+data_from_rankings.shape
+
+# %%
+data_from_rankings.sort(pl.col("avg_rating_p_value").abs()).head(10)
 
 # %%
 model_from_bayes_rankings = sm.OLS(
