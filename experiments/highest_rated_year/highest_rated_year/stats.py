592b2b2a14d171930168c8077b9d84149b8da8dc
Markus Schepke <markus.schepke@wolt.com>
2024-08-19 18:27:42 +0300

    Make use of sm.multipletests()

--- experiments/highest_rated_year/highest_rated_year/stats.py
+++ experiments/highest_rated_year/highest_rated_year/stats.py
@@ -17,43 +17,60 @@ def t_test(
     resid = influence.resid
     resid_studentized = influence.resid_studentized_internal
 
-    data = (
-        data.with_columns(
-            pl.Series(f"{y_column}_pred", predictions),
-            pl.Series(f"{y_column}_error", resid),
-            pl.Series(f"{y_column}_error_studentized", resid_studentized),
-            pl.Series(
-                f"{y_column}_p_value",
-                t.sf(x=np.abs(resid_studentized), df=model.df_resid) * 2,
-            ),
-        )
-        .with_columns(
-            (pl.col(f"{y_column}_p_value") <= sig_level).alias(
-                f"{y_column}_significant"
-            ),
-            (
-                sig_level
-                * pl.col(f"{y_column}_p_value").rank(method="ordinal")
-                / pl.len()
-            ).alias(f"{y_column}_bh_critical_value"),
-        )
-        .with_columns(
-            pl.when(
-                pl.col(f"{y_column}_p_value") < pl.col(f"{y_column}_bh_critical_value")
-            )
-            .then(f"{y_column}_p_value")
-            .otherwise(0)
-            .max()
-            .alias(f"{y_column}_bh_p_value")
-        )
-        .with_columns(
-            (pl.col(f"{y_column}_p_value") <= pl.col(f"{y_column}_bh_p_value")).alias(
-                f"{y_column}_bh_significant"
-            ),
-            (pl.col(f"{y_column}_p_value") <= sig_level / pl.len()).alias(
-                f"{y_column}_bonferroni_significant"
-            ),
-        )
+    data = data.with_columns(
+        pl.Series(f"{y_column}_pred", predictions),
+        pl.Series(f"{y_column}_error", resid),
+        pl.Series(f"{y_column}_error_studentized", resid_studentized),
+        pl.Series(
+            f"{y_column}_p_value",
+            t.sf(x=np.abs(resid_studentized), df=model.df_resid) * 2,
+        ),
+    ).with_columns(
+        (pl.col(f"{y_column}_p_value") <= sig_level).alias(f"{y_column}_significant"),
+    )
+
+    p_values = data[f"{y_column}_p_value"].to_numpy()
+    adjusted_bonferroni = sm.stats.multipletests(
+        pvals=p_values,
+        alpha=sig_level,
+        method="bonferroni",
+    )
+    adjusted_holm = sm.stats.multipletests(
+        pvals=p_values,
+        alpha=sig_level,
+        method="holm",
+    )
+    adjusted_bh = sm.stats.multipletests(
+        pvals=p_values,
+        alpha=sig_level,
+        method="fdr_bh",
+    )
+
+    data = data.with_columns(
+        pl.Series(
+            f"{y_column}_p_value_bonferroni",
+            adjusted_bonferroni[1],
+        ),
+        pl.Series(
+            f"{y_column}_significant_bonferroni",
+            adjusted_bonferroni[0],
+        ),
+        pl.Series(
+            f"{y_column}_p_value_holm",
+            adjusted_holm[1],
+        ),
+        pl.Series(
+            f"{y_column}_significant_holm",
+            adjusted_holm[0],
+        ),
+        pl.Series(
+            f"{y_column}_p_value_bh",
+            adjusted_bh[1],
+        ),
+        pl.Series(
+            f"{y_column}_significant_bh",
+            adjusted_bh[0],
+        ),
     )
 
     return data
