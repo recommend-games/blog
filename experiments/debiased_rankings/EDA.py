e60c2c6cc6030fc91d067bf89f7c908ed2c9e5de
Markus Schepke <markus.schepke@wolt.com>
2024-08-23 23:48:35 +0300

    First version of EDA.py

---
+++ experiments/debiased_rankings/EDA.py
@@ -0,0 +1,82 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+from datetime import date
+from pathlib import Path
+
+import jupyter_black
+import numpy as np
+import polars as pl
+from sklearn.preprocessing import MultiLabelBinarizer
+
+jupyter_black.load()
+pl.Config.set_tbl_rows(100)
+
+seed = 23
+this_year = date.today().year
+
+# %%
+base_dir = Path(".").resolve()
+project_dir = base_dir.parent.parent
+rankings_dir = project_dir.parent / "bgg-ranking-historicals"
+data_dir = project_dir.parent / "board-game-data"
+base_dir, project_dir, rankings_dir, data_dir
+
+# %%
+games = (
+    pl.scan_ndjson(data_dir / "scraped" / "bgg_GameItem.jl")
+    .filter(pl.col("year") >= 1970)
+    .filter(pl.col("year") <= this_year)
+    .filter(pl.col("complexity").is_not_null())
+    .filter(pl.col("rank").is_not_null())
+    .filter(pl.col("bgg_id") != 91313)  # Video game
+    .select(
+        "year",
+        "avg_rating",
+        "bayes_rating",
+        "complexity",
+        pl.col("cooperative").fill_null(False),
+        pl.col("game_type").fill_null([]),
+    )
+    .with_columns(age=this_year - pl.col("year"))
+    .collect()
+)
+games.shape
+
+# %%
+games.describe()
+
+# %%
+games.sample(10, seed=seed)
+
+# %%
+mlb = MultiLabelBinarizer()
+game_types_transformed = mlb.fit_transform(games["game_type"])
+game_types_transformed.shape
+
+# %%
+game_types = pl.DataFrame(
+    data=game_types_transformed.astype(np.int8),
+    schema=list(mlb.classes_),
+)
+game_types.describe()
+
+# %%
+data = pl.concat([games, game_types], how="horizontal")
+data.shape
+
+# %%
+data.sample(10, seed=seed)
