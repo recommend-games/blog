a9d43cee27ac69ab781c41834f7fbbe4e8cfb36c
Markus Shepherd <markus@recommend.games>
2025-02-27 17:32:28 +0200

    Sample games and users

--- experiments/election/Trust.py
+++ experiments/election/Trust.py
@@ -14,23 +14,88 @@
 # ---
 
 # %%
+import warnings
+from pathlib import Path
+
 import jupyter_black
 import numpy as np
+import polars as pl
 from board_game_recommender import LightGamesRecommender
 from election.schulze import compute_pairwise_preferences
 from election.trust import user_trust
 
 jupyter_black.load()
+rng = np.random.default_rng()
 
 # %%
-trust = user_trust("../../../board-game-data/scraped/bgg_RatingItem.jl", progress=True)
+PROJECT_DIR = Path(".").resolve().parent.parent
+
+DATA_DIR = PROJECT_DIR.parent / "board-game-data"
+RATINGS_FILE = DATA_DIR / "scraped" / "bgg_RatingItem.jl"
+
+SERVER_DIR = PROJECT_DIR.parent / "recommend-games-server"
+RECOMMENDER_FILE = SERVER_DIR / "data" / "recommender_light.npz"
+
+PROJECT_DIR, DATA_DIR, RATINGS_FILE, SERVER_DIR, RECOMMENDER_FILE
+
+# %%
+NUM_USERS = 100
+NUM_GAMES = 10
+NUM_USERS, NUM_GAMES
+
+# %%
+ratings_count = (
+    pl.scan_ndjson(RATINGS_FILE)
+    .filter(pl.col("bgg_user_rating").is_not_nan())
+    .group_by("bgg_id")
+    .agg(pl.len())
+    .with_columns(p_games=pl.col("len") / pl.sum("len"))
+    .sort("len", descending=True)
+    .collect()
+)
+ratings_count.head()
+
+# %%
+# Sample or top NUM_GAMES?
+sampled_games = rng.choice(
+    ratings_count["bgg_id"],
+    size=NUM_GAMES,
+    replace=False,
+    p=ratings_count["p_games"],
+    shuffle=False,
+)
+len(sampled_games)
+
+# %%
+with warnings.catch_warnings(action="ignore"):
+    trust = user_trust(str(RATINGS_FILE), progress=True)
+len(trust)
 
 # %%
 users, trust_scores = zip(*trust.items())
 trust_scores = np.array(trust_scores)
-p = trust_scores / trust_scores.sum()
+p_users = trust_scores / trust_scores.sum()
 trust_scores.shape
 
 # %%
-rng = np.random.default_rng()
-sampled_users = rng.choice(users, size=10, replace=False, p=p, shuffle=False)
+# Sample or top NUM_USERS?
+sampled_users = rng.choice(
+    users,
+    size=NUM_USERS,
+    replace=False,
+    p=p_users,
+    shuffle=False,
+)
+len(sampled_users)
+
+# %%
+recommender = LightGamesRecommender.from_npz(RECOMMENDER_FILE)
+recommender.num_games, recommender.num_users
+
+# %%
+# The whole matrix at once might be too large
+ratings_matrix = recommender.recommend_as_numpy(
+    users=sampled_users,
+    games=sampled_games,
+)
+ratings_matrix.shape
