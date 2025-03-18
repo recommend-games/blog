e22e5eb303fe5377ed5760590a7253f36ceef5a8
Markus Shepherd <markus@recommend.games>
2025-03-18 21:57:11 +0200

    Scrape more than one ranking page

--- experiments/bga_elo/bga_elo/bga_spider.py
+++ experiments/bga_elo/bga_elo/bga_spider.py
@@ -15,6 +15,7 @@ class BgaSpider(Spider):
         "CONCURRENT_REQUESTS_PER_DOMAIN": 4,
     }
 
+    max_ranking_pages = 10
     regex = re.compile("globalUserInfos=(.+);$", flags=re.MULTILINE)
 
     def parse(self, response: Response) -> Generator[dict | Request]:
@@ -45,13 +46,34 @@ class BgaSpider(Spider):
                         "mode": "elo",
                     },
                     callback=self.parse_ranking,
+                    meta={
+                        "game_id": game["id"],
+                        "start": 0,
+                    },
                 )
 
     def parse_ranking(self, response: Response) -> Generator[dict | Request]:
+        game_id = response.meta["game_id"]
+        start = response.meta["start"]
+
+        if start < self.max_ranking_pages - 1:
+            yield response.request.replace(
+                formdata={
+                    "game": str(game_id),
+                    "start": str(start + 1),
+                    "mode": "elo",
+                },
+                meta={
+                    "game_id": game_id,
+                    "start": start + 1,
+                },
+            )
+
         if not isinstance(response, TextResponse):
             return
 
         payload = response.json()
 
         for entry in payload["data"]["ranks"]:
+            entry["game_id"] = game_id
             yield entry
