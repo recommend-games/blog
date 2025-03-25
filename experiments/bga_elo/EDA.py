a879de17402f4e7fe4e333a1dae85b1869416473
Markus Shepherd <markus@recommend.games>
2025-03-25 22:59:41 +0200

    Better elo stats

--- experiments/bga_elo/EDA.py
+++ experiments/bga_elo/EDA.py
@@ -80,18 +80,20 @@ for k, v in games.partition_by("premium", maintain_order=False, as_dict=True).it
 # ## Rankings
 
 # %%
+schema = {
+    "id": pl.Int64,
+    "ranking": pl.Float64,
+    "nbr_game": pl.Int64,
+    "rank_no": pl.Int64,
+    "game_id": pl.Int64,
+}
+
+# %%
 rankings = (
-    pl.read_ndjson(
-        "results/rankings.jl",
-        schema_overrides={
-            "id": pl.Int64,
-            # "ranking": pl.Float64,
-            "nbr_game": pl.Int64,
-            "rank_no": pl.Int64,
-        },
-    )
-    .with_columns(pl.col("ranking").cast(pl.Float64))
+    pl.scan_ndjson("results/rankings.jl")
+    .select([pl.col(k).cast(v) for k, v in schema.items()])
     .with_columns(elo=pl.col("ranking") - 1300)
+    .collect()
 )
 rankings.shape
 
@@ -103,8 +105,63 @@ with pl.Config(tbl_rows=100):
     display(rankings.describe(percentiles=[0.01, 0.05, 0.25, 0.5, 0.75, 0.95, 0.99]))
 
 # %%
-rankings.group_by("game_id").agg(min_ranking=pl.col("ranking").min()).join(
-    games,
-    left_on="game_id",
-    right_on="id",
-).filter(~pl.col("is_ranking_disabled")).sort("min_ranking", descending=True).head(10)
+game_ranking_stats = rankings.group_by("game_id").agg(
+    num_players=pl.len(),
+    elo_mean=pl.col("elo").mean(),
+    elo_std=pl.col("elo").std(),
+    elo_min=pl.col("elo").min(),
+    elo_p01=pl.col("elo").quantile(0.01),
+    elo_p05=pl.col("elo").quantile(0.05),
+    elo_p25=pl.col("elo").quantile(0.25),
+    elo_p50=pl.col("elo").quantile(0.50),
+    elo_p75=pl.col("elo").quantile(0.75),
+    elo_p95=pl.col("elo").quantile(0.95),
+    elo_p99=pl.col("elo").quantile(0.99),
+    elo_max=pl.col("elo").max(),
+)
+game_info = (
+    games.filter(~pl.col("is_ranking_disabled"))
+    .filter(
+        ~pl.col("name").is_in(
+            ["vault", "thebrambles", "dicepyramid", "orchard", "grovesolitaire"]
+        )
+    )
+    .select(
+        "id",
+        "bgg_id",
+        "name",
+        "display_name_en",
+        "premium",
+        "weight",
+        "days_online",
+        "games_played",
+        "games_per_day",
+    )
+    .join(
+        game_ranking_stats,
+        left_on="id",
+        right_on="game_id",
+        how="inner",
+    )
+    .drop("id")
+    .with_columns(games_per_player=pl.col("games_played") / pl.col("num_players"))
+)
+games.shape, game_ranking_stats.shape, game_info.shape
+
+# %%
+game_info.sample(10, seed=seed)
+
+# %%
+game_info.describe()
+
+# %%
+game_info.sort("games_played", descending=True).head(10)
+
+# %%
+game_info.sort("num_players", descending=True).head(10)
+
+# %%
+game_info.sort("games_per_player", descending=True).head(10)
+
+# %%
+game_info.sort("elo_std", descending=True).head(10)
