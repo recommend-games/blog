81bd654617922f76cb8ba3df5d6f6365ee00bedf
Markus Shepherd <markus@recommend.games>
2025-03-31 22:11:41 +0300

    Sort merged files

--- experiments/bga_elo/bga_elo/data.py
+++ experiments/bga_elo/bga_elo/data.py
@@ -6,19 +6,18 @@ from board_game_merger.config import MergeConfig
 from board_game_merger.merge import merge_files
 
 
-def load_and_merge_rankings(
+def merge_rankings(
     rankings_path: str | Path | list[str] | list[Path],
     output_path: str | Path,
     progress_bar: bool = False,
 ) -> None:
     schema = {
-        "id": pl.Int64,
         "name": pl.String,
-        "ranking": pl.Float64,
-        "nbr_game": pl.Int64,
-        "rank_no": pl.Int64,
+        "ranking": pl.String,
+        "nbr_game": pl.String,
+        "rank_no": pl.String,
         "game_id": pl.Int64,
-        "scraped_at": pl.Datetime,
+        "scraped_at": pl.String,
     }
 
     config = MergeConfig(
@@ -26,7 +25,13 @@ def load_and_merge_rankings(
         in_paths=rankings_path,
         out_path=output_path,
         key_col=["game_id", "name"],
-        latest_col="scraped_at",
+        latest_col=pl.col("scraped_at").str.to_datetime(time_zone="UTC"),
+        sort_fields=[
+            "game_id",
+            -pl.col("ranking").cast(pl.Float64),
+            pl.col("rank_no").cast(pl.Float64),
+            "name",
+        ],
     )
 
     merge_files(
@@ -35,4 +40,12 @@ def load_and_merge_rankings(
         drop_empty=True,
         sort_keys=True,
         progress_bar=progress_bar,
+    )
+
+
+if __name__ == "__main__":
+    merge_rankings(
+        rankings_path="results/rankings-*.jl",
+        output_path="rankings.jl",
+        progress_bar=True,
     )
