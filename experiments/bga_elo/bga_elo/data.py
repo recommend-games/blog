3026b648f8d9211f41bcb3c0ed3f6c8d721ec73b
Markus Shepherd <markus@recommend.games>
2025-03-31 22:18:33 +0300

    Added merge_games()

--- experiments/bga_elo/bga_elo/data.py
+++ experiments/bga_elo/bga_elo/data.py
@@ -6,9 +6,51 @@ from board_game_merger.config import MergeConfig
 from board_game_merger.merge import merge_files
 
 
+def merge_games(
+    games_path: str | Path | list[str] | list[Path],
+    output_path: str | Path,
+    overwrite: bool = False,
+    progress_bar: bool = False,
+) -> None:
+    schema = {
+        "id": pl.Int64,
+        "name": pl.String,
+        "display_name_en": pl.String,
+        "status": pl.String,
+        "premium": pl.Boolean,
+        "locked": pl.Boolean,
+        "weight": pl.Int64,
+        "priority": pl.Int64,
+        "games_played": pl.Int64,
+        "published_on": pl.String,
+        "average_duration": pl.Int64,
+        "bgg_id": pl.Int64,
+        "is_ranking_disabled": pl.Boolean,
+        "scraped_at": pl.String,
+    }
+
+    config = MergeConfig(
+        schema=pl.Schema(schema),
+        in_paths=games_path,
+        out_path=output_path,
+        key_col="id",
+        latest_col=pl.col("scraped_at").str.to_datetime(time_zone="UTC"),
+        sort_fields="id",
+    )
+
+    merge_files(
+        merge_config=config,
+        overwrite=overwrite,
+        drop_empty=True,
+        sort_keys=True,
+        progress_bar=progress_bar,
+    )
+
+
 def merge_rankings(
     rankings_path: str | Path | list[str] | list[Path],
     output_path: str | Path,
+    overwrite: bool = False,
     progress_bar: bool = False,
 ) -> None:
     schema = {
@@ -36,7 +78,7 @@ def merge_rankings(
 
     merge_files(
         merge_config=config,
-        overwrite=False,
+        overwrite=overwrite,
         drop_empty=True,
         sort_keys=True,
         progress_bar=progress_bar,
@@ -44,8 +86,15 @@ def merge_rankings(
 
 
 if __name__ == "__main__":
+    merge_games(
+        games_path="results/games-*.jl",
+        output_path="games.jl",
+        overwrite=True,
+        progress_bar=True,
+    )
     merge_rankings(
         rankings_path="results/rankings-*.jl",
         output_path="rankings.jl",
+        overwrite=True,
         progress_bar=True,
     )
