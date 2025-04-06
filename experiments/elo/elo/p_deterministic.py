de10aa88ddb7eee283037cab139d7bbe9dabcf89
Markus Shepherd <markus@recommend.games>
2025-04-06 21:21:03 +0300

    First versions of elo_probability() and update_elo_scores_p_deterministic()

--- experiments/elo/elo/p_deterministic.py
+++ experiments/elo/elo/p_deterministic.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from collections.abc import Iterable
 import numpy as np
 
 
@@ -20,3 +21,52 @@ def simulate_p_deterministic_games(
     player_a_wins = np.where(deterministic, det_player_a_wins, prob_player_a_wins)
 
     return players_a, players_b, player_a_wins
+
+
+def elo_probability(diff: float, scale: float = 400) -> float:
+    return 1 / (1 + 10 ** (-diff / scale))
+
+
+def update_elo_scores_p_deterministic(
+    rng: np.random.Generator,
+    elo_scores: np.ndarray,
+    num_games: int,
+    p_deterministic: float,
+    elo_k: float = 32,
+    elo_scale: float = 400,
+    inplace: bool = True,
+    progress_bar: bool = False,
+) -> np.ndarray:
+    assert elo_scores.ndim == 1
+    num_players = len(elo_scores)
+
+    if not inplace:
+        elo_scores = np.copy(elo_scores)
+
+    players_a, players_b, player_a_wins = simulate_p_deterministic_games(
+        rng=rng,
+        num_players=num_players,
+        num_games=num_games,
+        p_deterministic=p_deterministic,
+    )
+
+    if progress_bar:
+        from tqdm import trange
+
+        iterator: Iterable[int] = trange(num_games)
+
+    else:
+        iterator = range(num_games)
+
+    for i in iterator:
+        player_a = players_a[i]
+        elo_a = elo_scores[player_a]
+        player_b = players_b[i]
+        elo_b = elo_scores[player_b]
+        prob_a_wins = elo_probability(diff=elo_a - elo_b, scale=elo_scale)
+        outcome_a_wins = player_a_wins[i]
+        elo_update = elo_k * (outcome_a_wins - prob_a_wins)
+        elo_scores[player_a] += elo_update
+        elo_scores[player_b] -= elo_update
+
+    return elo_scores
