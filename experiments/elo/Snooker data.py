a88c30532e7ac94a14dfddee89b526f33f4c1263
Markus Shepherd <markus@recommend.games>
2025-04-08 21:26:45 +0300

    First version of Snooker data.py

---
+++ experiments/elo/Snooker data.py
@@ -0,0 +1,64 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import jupyter_black
+import polars as pl
+
+jupyter_black.load()
+
+seed = 13
+
+# %%
+events = (
+    pl.scan_ndjson("results/snooker/events*.jl")
+    .sort("scraped_at")
+    .group_by("ID", maintain_order=True)
+    .last()
+    .collect()
+)
+matches = (
+    pl.scan_ndjson("results/snooker/matches*.jl")
+    .sort("scraped_at")
+    .group_by("ID", maintain_order=True)
+    .last()
+    .collect()
+)
+players = (
+    pl.scan_ndjson("results/snooker/players*.jl")
+    .sort("scraped_at")
+    .group_by("ID", maintain_order=True)
+    .last()
+    .collect()
+)
+events.shape, matches.shape, players.shape
+
+# %%
+events.sample(10, seed=seed)
+
+# %%
+events.describe()
+
+# %%
+matches.sample(10, seed=seed)
+
+# %%
+matches.describe()
+
+# %%
+players.sample(10, seed=seed)
+
+# %%
+players.describe()
