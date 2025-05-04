ae26b1107dc603e05777af8075359d83064187e0
Markus Shepherd <markus@recommend.games>
2025-05-04 20:49:43 +0300

    Updated update_elo_ratings_p_deterministic()

--- experiments/elo/elo/p_deterministic.py
+++ experiments/elo/elo/p_deterministic.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
-from collections import defaultdict
 import numpy as np
 
-from elo.elo_ratings import calculate_elo_ratings
+from elo.elo_ratings import EloRatingSystem, TwoPlayerElo, RankOrderedLogitElo
 
 
 def simulate_p_deterministic_matches(
@@ -45,44 +44,33 @@ def simulate_p_deterministic_matches(
 
 
 def update_elo_ratings_p_deterministic(
+    *,
     rng: np.random.Generator,
-    elo_ratings: np.ndarray,
-    num_games: int,
+    elo: EloRatingSystem | None = None,
+    num_players: int,
+    num_matches: int,
+    players_per_match: int = 2,
     p_deterministic: float,
-    elo_initial: float = 0,
-    elo_k: float = 32,
-    elo_scale: float = 400,
-    inplace: bool = True,
     progress_bar: bool = False,
-) -> np.ndarray:
-    assert elo_ratings.ndim == 1
-    num_players = len(elo_ratings)
+) -> EloRatingSystem:
+    if elo is None:
+        elo = TwoPlayerElo() if players_per_match == 2 else RankOrderedLogitElo()
 
-    if not inplace:
-        elo_ratings = np.copy(elo_ratings)
-
-    players_a, players_b, player_a_wins = simulate_p_deterministic_matches(
+    matches = simulate_p_deterministic_matches(
         rng=rng,
         num_players=num_players,
-        num_matches=num_games,
+        num_matches=num_matches,
+        players_per_match=players_per_match,
         p_deterministic=p_deterministic,
     )
 
-    results = calculate_elo_ratings(
-        player_1_ids=players_a,
-        player_2_ids=players_b,
-        player_1_outcomes=player_a_wins,
-        init_elo_ratings=dict(enumerate(elo_ratings)),
-        elo_initial=elo_initial,
-        elo_k=elo_k,
-        elo_scale=elo_scale,
+    payoffs = np.arange(players_per_match - 1, -1, -1, dtype=float)
+
+    elo.update_elo_ratings_batch(
+        matches=(dict(zip(match, payoffs)) for match in matches),
         full_results=False,
         progress_bar=progress_bar,
+        tqdm_kwargs={"total": num_matches},
     )
 
-    assert isinstance(results, defaultdict)
-
-    for player_id, elo_rating in results.items():
-        elo_ratings[player_id] = elo_rating
-
-    return elo_ratings
+    return elo
