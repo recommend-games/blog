378a7872f135a5700765df3ab70272bec0acfabe
Markus Shepherd <markus@recommend.games>
2025-10-22 23:28:24 +0300

    Added permutations

--- experiments/elo/src/lib.rs
+++ experiments/elo/src/lib.rs
@@ -1,4 +1,33 @@
+use once_cell::sync::Lazy;
 use pyo3::prelude::*;
+
+pub static PERMS: Lazy<Vec<Vec<Vec<usize>>>> = Lazy::new(|| {
+    // PERMS[n] = all permutations of 0..n-1, for n up to 6
+    let mut out: Vec<Vec<Vec<usize>>> = vec![vec![]; 7];
+    for n in 0..=6 {
+        let mut perms = Vec::new();
+        let mut a: Vec<usize> = (0..n).collect();
+        heap_permute(n, &mut a, &mut perms);
+        out[n] = perms;
+    }
+    out
+});
+
+fn heap_permute(k: usize, a: &mut [usize], out: &mut Vec<Vec<usize>>) {
+    if k <= 1 {
+        out.push(a.to_vec());
+        return;
+    }
+    heap_permute(k - 1, a, out);
+    for i in 0..(k - 1) {
+        if k % 2 == 0 {
+            a.swap(i, k - 1);
+        } else {
+            a.swap(0, k - 1);
+        }
+        heap_permute(k - 1, a, out);
+    }
+}
 
 fn hello_core() -> String {
     "Hello from example-ext!".to_string()
--- experiments/elo/src/main.rs
+++ experiments/elo/src/main.rs
@@ -1,5 +1,11 @@
-use elo::hello_for_rust;
+use elo::{hello_for_rust, PERMS};
 
 fn main() {
     println!("{}", hello_for_rust());
+    for n in 0..=6 {
+        println!("n={} perms={}", n, PERMS[n].len());
+        for p in &PERMS[n] {
+            println!("{:?}", p);
+        }
+    }
 }
