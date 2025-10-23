f08c5e328c528a9bfe3e7cdc1719b351466a1b5b
Markus Shepherd <markus@recommend.games>
2025-10-23 22:28:28 +0300

    Added elo.__main__

--- experiments/elo/python/elo/__init__.py
+++
@@ -1,5 +0,0 @@
-from elo._rust import hello_from_bin
-
-
-def main() -> None:
-    print(hello_from_bin())
---
+++ experiments/elo/python/elo/__main__.py
@@ -0,0 +1,9 @@
+from elo._rust import hello_from_bin
+
+
+def main() -> None:
+    print(hello_from_bin())
+
+
+if __name__ == "__main__":
+    main()
