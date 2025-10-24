88b94593a8656f979f2089c026f2800c50f5c14d
Markus Shepherd <markus@recommend.games>
2025-10-24 16:08:58 +0300

    Change __main__

--- experiments/elo/python/elo/__main__.py
+++ experiments/elo/python/elo/__main__.py
@@ -1,8 +1,56 @@
-from elo._rust import hello_from_bin
+import polars as pl
+import numpy as np
+from numpy import typing as npt
+import sys
+from elo._rust import calculate_elo_ratings_rust
+from elo.elo_ratings import calculate_elo_ratings_python
 
 
-def main() -> None:
-    print(hello_from_bin())
+def matches_from_arrow_file(file_path: str) -> npt.NDArray[np.int32]:
+    data = (
+        pl.scan_ipc(file_path)
+        .select(pl.col("player_ids").list.to_struct(upper_bound=2))
+        .unnest("player_ids")
+        .drop_nulls()
+        .collect()
+    )
+    print(f"Loaded {len(data)} matches from {file_path}")
+    return data.to_numpy().astype(np.int32)
+
+
+def main():
+    matches = matches_from_arrow_file(sys.argv[1])
+
+    elo_initial = 0.0
+    elo_k = 32.0
+    elo_scale = 400.0
+
+    print("Elo ratings (Python implementation):")
+    elo_ratings = calculate_elo_ratings_python(
+        matches=matches,
+        elo_initial=elo_initial,
+        elo_k=elo_k,
+        elo_scale=elo_scale,
+        progress_bar=True,
+    )
+    sorted_ratings = sorted(elo_ratings.items(), key=lambda x: x[1], reverse=True)
+    if len(sorted_ratings) > 20:
+        sorted_ratings = sorted_ratings[:10] + [(0, 0)] + sorted_ratings[-10:]
+    for player, rating in sorted_ratings:
+        print(f"Player {player:4d}:\tElo {rating:10.3f}")
+
+    print("Elo ratings (Rust implementation):")
+    elo_ratings = calculate_elo_ratings_rust(
+        matches=matches,
+        elo_initial=elo_initial,
+        elo_k=elo_k,
+        elo_scale=elo_scale,
+    )
+    sorted_ratings = sorted(elo_ratings.items(), key=lambda x: x[1], reverse=True)
+    if len(sorted_ratings) > 20:
+        sorted_ratings = sorted_ratings[:10] + [(0, 0)] + sorted_ratings[-10:]
+    for player, rating in sorted_ratings:
+        print(f"Player {player:4d}:\tElo {rating:10.3f}")
 
 
 if __name__ == "__main__":
