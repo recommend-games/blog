0458501e91e72bd85fa9603295e276c4832aaebd
Markus Shepherd <markus@recommend.games>
2025-10-24 20:49:10 +0300

    Added timer

--- experiments/elo/python/elo/__main__.py
+++ experiments/elo/python/elo/__main__.py
@@ -19,6 +19,8 @@ def matches_from_arrow_file(file_path: str) -> npt.NDArray[np.int32]:
 
 
 def main():
+    from codetiming import Timer
+
     matches = matches_from_arrow_file(sys.argv[1])
 
     elo_initial = 0.0
@@ -26,31 +28,33 @@ def main():
     elo_scale = 400.0
 
     print("Elo ratings (Python implementation):")
-    elo_ratings = calculate_elo_ratings_python(
-        matches=matches,
-        elo_initial=elo_initial,
-        elo_k=elo_k,
-        elo_scale=elo_scale,
-        progress_bar=True,
-    )
+    with Timer(text="Calculated Elo ratings (Python) in {:.3f} seconds", logger=print):
+        elo_ratings = calculate_elo_ratings_python(
+            matches=matches,
+            elo_initial=elo_initial,
+            elo_k=elo_k,
+            elo_scale=elo_scale,
+            progress_bar=True,
+        )
     sorted_ratings = sorted(elo_ratings.items(), key=lambda x: x[1], reverse=True)
     if len(sorted_ratings) > 20:
         sorted_ratings = sorted_ratings[:10] + [(0, 0)] + sorted_ratings[-10:]
     for player, rating in sorted_ratings:
-        print(f"Player {player:4d}:\tElo {rating:10.3f}")
+        print(f"Player {player:10d}:\tElo {rating:10.3f}")
 
     print("Elo ratings (Rust implementation):")
-    elo_ratings = calculate_elo_ratings_rust(
-        matches=matches,
-        elo_initial=elo_initial,
-        elo_k=elo_k,
-        elo_scale=elo_scale,
-    )
+    with Timer(text="Calculated Elo ratings (Rust) in {:.3f} seconds", logger=print):
+        elo_ratings = calculate_elo_ratings_rust(
+            matches=matches,
+            elo_initial=elo_initial,
+            elo_k=elo_k,
+            elo_scale=elo_scale,
+        )
     sorted_ratings = sorted(elo_ratings.items(), key=lambda x: x[1], reverse=True)
     if len(sorted_ratings) > 20:
         sorted_ratings = sorted_ratings[:10] + [(0, 0)] + sorted_ratings[-10:]
     for player, rating in sorted_ratings:
-        print(f"Player {player:4d}:\tElo {rating:10.3f}")
+        print(f"Player {player:10d}:\tElo {rating:10.3f}")
 
 
 if __name__ == "__main__":
