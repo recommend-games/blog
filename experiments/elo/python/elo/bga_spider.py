63f0c3bbb6dfa79bb543e81b2431d03382b7ff8f
Markus Shepherd <markus@recommend.games>
2026-02-20 10:33:33 +0200

    Yeah, we will not move utils to utils after all

--- experiments/elo/python/elo/bga_spider.py
+++ experiments/elo/python/elo/bga_spider.py
@@ -14,15 +14,45 @@ import jmespath
 from scrapy import FormRequest, Request, Selector, Spider
 from scrapy.http import Response, TextResponse
 
-from elo.utils import (
-    _env_allow_list_ids,
-    _env_bool,
-    _env_int,
-    _env_int_optional,
-)
-
 if TYPE_CHECKING:
     from collections.abc import Container, Generator, Iterable
+
+
+def _env_bool(name: str, default: bool) -> bool:
+    val = os.environ.get(name)
+    if val is None:
+        return default
+    return val.strip().lower() in ("1", "true", "yes")
+
+
+def _env_int(name: str, default: int) -> int:
+    val = os.environ.get(name)
+    if val is None or not val.strip():
+        return default
+    try:
+        return int(val.strip())
+    except ValueError:
+        return default
+
+
+def _env_int_optional(name: str) -> int | None:
+    val = os.environ.get(name)
+    if val is None or not val.strip():
+        return None
+    try:
+        return int(val.strip())
+    except ValueError:
+        return None
+
+
+def _env_allow_list_ids(name: str) -> frozenset[int] | None:
+    val = os.environ.get(name)
+    if val is None or not val.strip():
+        return None
+    try:
+        return frozenset(int(x.strip()) for x in val.split(",") if x.strip())
+    except ValueError:
+        return None
 
 
 class BaseFilter:
